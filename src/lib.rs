0167a04f9ef3c61f731ea96911fde6ad15e24957
Zack Slayton <zack.slayton@gmail.com>
2015-06-14 23:52:13 -0400

    Version set.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
-#[test]
-fn it_works() {
+#![allow(dead_code)]
+use std::collections::BTreeSet;
+
+pub struct CronExpression {
+  // The original String that was parsed
+  expression: String,
+  // Schedule information
+  seconds: BTreeSet<usize>,
+  minutes: BTreeSet<usize>, 
+  hours: BTreeSet<usize>, 
+  days_of_month: BTreeSet<usize>, 
+  months: BTreeSet<usize>, 
+  days_of_week: BTreeSet<usize>, 
+  years: BTreeSet<usize>, 
 }
