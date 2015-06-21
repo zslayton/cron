1217b97e7740f02419d61bf8109e19c060137290
Zack Slayton <zack.slayton@gmail.com>
2015-06-20 20:46:34 -0400

    Removed unnecessary mut.

--- tests/lib.rs
+++ tests/lib.rs
@@ -21,7 +21,7 @@ mod tests {
   #[test]
   fn test_parse_with_lists() {
     let expression = "1 2,17,51 1-3,6,9-11 4,29 2,3,7 6 2015-2017";
-    let mut schedule = CronSchedule::parse(expression).unwrap();
+    let schedule = CronSchedule::parse(expression).unwrap();
     let mut date = UTC::now();
     println!("Fire times for {}:", expression);
     for _ in 0..20 {
@@ -34,7 +34,7 @@ mod tests {
   #[test]
   fn test_upcoming_iterator() {
     let expression = "2,17,51 1-3,6,9-11 4,29 2,3,7 6 2015-2017";
-    let mut schedule = CronSchedule::parse(expression).unwrap();
+    let schedule = CronSchedule::parse(expression).unwrap();
     println!("Upcoming fire times for '{}':", expression);
     for datetime in schedule.upcoming().take(12) {
       println!("-> {}", datetime);
@@ -63,7 +63,7 @@ mod tests {
   #[test]
   fn test_next_utc() {
     let expression = "1 2 3 4 10 6 2015,2016,2017,2018";
-    let mut schedule = CronSchedule::parse(expression).unwrap();
+    let schedule = CronSchedule::parse(expression).unwrap();
     let next = schedule.next_utc().expect("There was no upcoming fire time.");
     println!("Next fire time: {}", next.to_rfc3339());
   }
