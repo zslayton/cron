4919ce6321ef72ba36a30a144db364f97aad08d9
Zack Slayton <zack.slayton@gmail.com>
2017-08-03 21:13:37 -0400

    Examples now use correct capitalization of Utc

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@
 //! extern crate cron;
 //!
 //! use cron::Schedule;
-//! use chrono::UTC;
+//! use chrono::Utc;
 //! use std::str::FromStr;
 //! 
 //! fn main() {
@@ -19,7 +19,7 @@
 //!   let expression = "0   30   9,12,15     1,15       May-Aug  Mon,Wed,Fri  2018/2";
 //!   let schedule = Schedule::from_str(expression).unwrap();
 //!   println!("Upcoming fire times:");
-//!   for datetime in schedule.upcoming(UTC).take(10) {
+//!   for datetime in schedule.upcoming(Utc).take(10) {
 //!     println!("-> {}", datetime);
 //!   }
 //! }
